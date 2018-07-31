30b860c300bf4139a66ed28e76d07bd48619655c
Ekanshi Agrawal <ekanshi@gmail.com>
2018-07-31 16:40:00 +0530

    Fix script rendering

--- script.js
+++ script.js
@@ -4,33 +4,35 @@ if (localStorage.getItem('tasks') !== null) {
   allTasks = JSON.parse(localStorage.getItem('tasks'))
 }
 
-var app = new Vue({
-  el: '#to-do-app',
-  data: {
-    allTasks: allTasks
-  },
-  methods: {
-    addNewTask: function (task) {
-      allTasks.push({'desc': task, 'category': 'open', 'notes': '', 'dueDate': '', 'priority': ''})
+window.addEventListener('load', () => {
+  var app = new Vue({
+    el: '#to-do-app',
+    data: {
+      allTasks: allTasks
+    },
+    methods: {
+      addNewTask: function (task) {
+        allTasks.push({'desc': task, 'category': 'open', 'notes': '', 'dueDate': '', 'priority': ''})
+      }
     }
-  }
+  })
 })
 
-Vue.component('taskDiv', {
+Vue.component('task-div', {
   props: ['task'],
   template: `
-		<input type= "checkbox" id="taskChecked" value={{taskValue}}>
+		<input type= "checkbox" id="taskChecked" :value="taskValue">
 		<p v-model="taskValue">{{task.desc}}</p>
 		<button type="button" id="openOptions">^</button>
-		<optionDiv>
+		<option-div>
 			v-bind:notes = task.notes
 			v-bind:dueBy = task.dueDate
 			v-bind:priority = task.priority
-		</optionDiv>
+		</option-div>
 	`
 })
 
-Vue.component('optionDiv', {
+Vue.component('option-div', {
   props: ['notes', 'dueDate', 'priority'],
   template: `
 		<label for="taskNotes">Notes</label>
