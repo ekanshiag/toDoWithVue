264bd8a787b26f3e7ab4ed2bfb259d2709aac90f
Ekanshi Agrawal <ekanshi@gmail.com>
2018-07-31 16:58:21 +0530

    Fix components template

--- script.js
+++ script.js
@@ -21,31 +21,35 @@ window.addEventListener('load', () => {
 Vue.component('task-div', {
   props: ['task'],
   template: `
-		<input type= "checkbox" id="taskChecked" :value="taskValue">
-		<p v-model="taskValue">{{task.desc}}</p>
-		<button type="button" id="openOptions">^</button>
-		<option-div>
-			v-bind:notes = task.notes
-			v-bind:dueBy = task.dueDate
-			v-bind:priority = task.priority
-		</option-div>
+  		<div>
+			<input type= "checkbox" id="taskChecked">
+			<p v-model="taskValue">{{task.desc}}</p>
+			<button type="button" id="openOptions">^</button>
+			<option-div>
+				v-bind:notes = task.notes
+				v-bind:dueBy = task.dueDate
+				v-bind:priority = task.priority
+			</option-div>
+		</div>
 	`
 })
 
 Vue.component('option-div', {
   props: ['notes', 'dueDate', 'priority'],
   template: `
-		<label for="taskNotes">Notes</label>
-		<p id="taskNotes">{{notes}}</p>
-		<label for="dueDate">Due date</label>
-		<input id="dueDate" type="Date" v-bind:value=dueDate>
-		<label for="priority">Priority</label>
-		<select value=priority>
-		<option disabled value="">Please select one</option>
-		<option value="low">Low</option>
-		<option value="Medium">Medium</option>
-		<option value="High">High</option>
-		</select>
-		<button v-on:click="">Delete</button>
+  		<div>
+			<label for="taskNotes">Notes</label>
+			<textarea id="taskNotes">{{notes}}</textarea>
+			<label for="dueDate">Due date</label>
+			<input id="dueDate" type="Date" v-bind:value=dueDate>
+			<label for="priority">Priority</label>
+			<select value=priority>
+				<option disabled value="">Please select one</option>
+				<option value="low">Low</option>
+				<option value="Medium">Medium</option>
+				<option value="High">High</option>
+			</select>
+			<button v-on:click="">Delete</button>
+		</div>
 	`
 })
